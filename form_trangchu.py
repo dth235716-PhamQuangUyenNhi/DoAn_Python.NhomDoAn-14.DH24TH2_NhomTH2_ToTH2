4d315f8b725394ead330108320006a8605c01962
dth235716-PhamQuangUyenNhi <nhi_dth235716@student.agu.edu.vn>
2025-11-17 09:21:21 +0700

    them ttgv 17/11

--- form_trangchu.py
+++ form_trangchu.py
@@ -37,53 +37,6 @@ def main():
   btn_giaovien = tk.Button(frame_buttons, text="Thông tin giáo viên", width=15, command=xem_thong_tin_giaovien)
   btn_giaovien.pack(side="left", padx=5)
 
-  c
-
-
-  def tim_giaovien():
-    form_timkiem.main()  # Mở form tim kiếm giáo viên
-
-  def Lich_phancong():
-    form_phancong.main()  # Mở form phân công
-import tkinter as tk
-from tkinter import ttk, messagebox
-import DEMO_CT_QLGVPT
-import form_timkiem
-import form_phancong
-import form_thongtinbomon
-
-# căn giữa cửa sổ
-def center_window(win, w=300, h=600):
-  ws = win.winfo_screenwidth()
-  hs = win.winfo_screenheight()
-  x = (ws // 2) - (w // 2)
-  y = (hs // 2) - (h // 2)
-  win.geometry(f'{w}x{h}+{x}+{y}')
-
-
-def main():
-  trangchu = tk.Tk()
-  trangchu.title("Chương trình quản lý giáo viên phổ thông")
-  center_window(trangchu, 600, 300)
-  trangchu.resizable(False, False)
-
-  # Frame chứa 4 nút nằm ngang
-  frame_buttons = tk.Frame(trangchu)
-  frame_buttons.pack(anchor='nw', pady=10, padx=10)
-
-
-
-  # Nút 1: "Trang Chủ", disabled
-  btn_trangchu = tk.Button(frame_buttons, text="Trang Chủ", state="disabled", width=10)
-  btn_trangchu.pack(side="left", padx=5)
-
-  def xem_thong_tin_giaovien():
-    DEMO_CT_QLGVPT.main()  # Mở form trang chủ
-
-  # Nút 2: "Xem thông tin giáo viên"
-  btn_giaovien = tk.Button(frame_buttons, text="Thông tin giáo viên", width=15, command=xem_thong_tin_giaovien)
-  btn_giaovien.pack(side="left", padx=5)
-
 
   def tim_giaovien():
     form_timkiem.main()  # Mở form tim kiếm giáo viên
